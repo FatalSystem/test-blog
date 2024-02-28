d47f6236e4fa37e9fd149d6c896b538fe7df85cd
Jassel <ohjjmm@gmail.com>
2024-02-27 21:33:48 -0400

    feat: add sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
-import { defineConfig } from "astro/config"
-import tailwind from "@astrojs/tailwind"
-import { astroImageTools } from "astro-imagetools"
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import { astroImageTools } from 'astro-imagetools'
+import react from '@astrojs/react'
 
-import react from "@astrojs/react"
+import { sanityIntegration } from '@sanity/astro'
 
 // https://astro.build/config
 export default defineConfig({
   // experimental: {
   //   assets: true,
   // },
-  integrations: [tailwind(), react({ experimentalReactChildren: true}), astroImageTools],
+  output: 'hybrid',
+  integrations: [tailwind(), react({
+    experimentalReactChildren: true
+  }), astroImageTools, sanityIntegration({
+    projectId: '3nx5ege4',
+    dataset: 'production',
+    useCdn: false,
+    studioBasePath: '/admin'
+  })]
 })
---
+++ src/schemas/question.ts
@@ -0,0 +1,17 @@
+export default {
+  name: 'questions',
+  type: 'document',
+  title: 'Questions',
+  fields: [
+    {
+      name: 'Question',
+      type: 'string',
+      title: 'Question'
+    },
+    {
+      name: 'Answer',
+      type: 'string',
+      title: 'Answer'
+    }
+  ]
+}
