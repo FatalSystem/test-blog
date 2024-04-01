bd606cd0ddf21936992fbe645861a8f34bd6331e
Jassel <ohjjmm@gmail.com>
2024-04-01 14:55:24 -0400

    improv: enrforce full width for mobile buttons

--- .netlify/functions-internal/ssr/chunks/generic_DxPzXJPj.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/generic_CWcf_uCc.mjs').then(n => n.g);
-
-export { page };
--- .netlify/functions-internal/ssr/chunks/studio-route_XYE-ISyV.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('./pages/studio-route_9IDS_YvP.mjs').then(n => n.a);
-
-export { page };
