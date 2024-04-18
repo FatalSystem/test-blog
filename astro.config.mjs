b1363f5f55db12e26f94498320cd5c397029e6c5
Jassel <ohjjmm@gmail.com>
2024-04-18 19:03:08 -0400

    bugfix: fix T5setup redirection slug

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
   redirects: {
     '/how-it-works/': '/rover/',
     '/call': 'https://calendly.com/haithameletrabi/introcall',
-    '/t5setup': 'https://youtu.be/BerTwqWqAPo',
+    '/T5setup': 'https://youtu.be/BerTwqWqAPo',
     '/unbox': 'https://youtu.be/vvNnUARbGz0',
     '/quickstart': 'https://www.youtube.com/watch?v=c7O39UYMls4'
   },
