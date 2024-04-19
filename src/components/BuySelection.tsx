e7f7e0e39f315082cd91d387e33a06197ab0fc0c
Jassel <ohjjmm@gmail.com>
2024-04-19 18:40:20 -0400

    chore: buy page changes + add edgetag script

--- src/components/BuySelection.tsx
+++ src/components/BuySelection.tsx
@@ -1,81 +1,53 @@
 import { useEffect, useState } from 'react'
 import { Switch } from './Switch'
 import { motion } from 'framer-motion'
-import ReactGA from 'react-ga4'
 
 const titleStyle = 'text-t-off-white text-left font-avenirBold text-xl md:text-base lg:text-xl xl:text-2xl mb-4'
 const itemStyle = 'text-sm md:text-xs xl:text-sm 2xl:text-base'
 
 export default function BuySelection (): JSX.Element {
-  const [isChecked, setIsChecked] = useState<boolean>(false)
+  // const [isChecked, setIsChecked] = useState<boolean>(false)
   const [selected, setSelected] = useState<number>(0)
 
   const handleNavigation = (product: number): void => {
-    ReactGA.event({
-      category: 'set_checkout_option',
-      action: 'set_checkout_option',
-      value: 0
-    })
-
     if (product === 0) {
-      if (isChecked) {
-        ReactGA.event({
-          category: 'set_checkout_option',
-          action: 'set_checkout_option',
-          value: 1
-        })
-        window.location.href = 'https://store.tennibot.com/tools/recurring/checkout_link?magic=eyJpdGVtcyI6IFt7ImlkIjogNDQzMTEyOTExNjY4ODMsICJxdWFudGl0eSI6IDEsICJzZWxsaW5nX3BsYW4iOiAxNzE2OTEyMjkxLCAic2VsbGluZ19wbGFuX2dyb3VwX2lkIjogODg2NDQwMDk5fV19&store_id=190268'
-        return
-      }
-      ReactGA.event({
-        category: 'set_checkout_option',
-        action: 'set_checkout_option',
-        value: 2
-      })
-      window.location.href = 'https://store.tennibot.com/tools/recurring/checkout_link?magic=eyJpdGVtcyI6IFt7ImlkIjogNDQzMTEyNzQ0MjI0MzUsICJxdWFudGl0eSI6IDEsICJzZWxsaW5nX3BsYW4iOiAxNzE2ODc5NTIzLCAic2VsbGluZ19wbGFuX2dyb3VwX2lkIjogODg2NDA3MzMxfV19&store_id=190268'
+      // if (isChecked) {
+      //   ReactGA.event({
+      //     category: 'set_checkout_option',
+      //     action: 'set_checkout_option',
+      //     value: 1
+      //   })
+      //   window.location.href = 'https://store.tennibot.com/tools/recurring/checkout_link?magic=eyJpdGVtcyI6IFt7ImlkIjogNDQzMTEyOTExNjY4ODMsICJxdWFudGl0eSI6IDEsICJzZWxsaW5nX3BsYW4iOiAxNzE2OTEyMjkxLCAic2VsbGluZ19wbGFuX2dyb3VwX2lkIjogODg2NDQwMDk5fV19&store_id=190268'
+      //   return
+      // }
+      window.location.href = 'https://store.tennibot.com/tools/recurring/checkout_link?magic=eyJpdGVtcyI6IFt7ImlkIjogNDQzMTEyOTExNjY4ODMsICJxdWFudGl0eSI6IDEsICJzZWxsaW5nX3BsYW4iOiAxNzE2OTEyMjkxLCAic2VsbGluZ19wbGFuX2dyb3VwX2lkIjogODg2NDQwMDk5fV19&store_id=190268'
       return
     }
 
     if (product === 1) {
-      ReactGA.event({
-        category: 'set_checkout_option',
-        action: 'set_checkout_option',
-        value: 3
-      })
-      window.location.href = 'https://store.tennibot.com/tools/recurring/checkout_link?magic=eyJpdGVtcyI6IFt7ImlkIjogNDQzMDE0ODM5MDEwOTEsICJxdWFudGl0eSI6IDEsICJzZWxsaW5nX3BsYW4iOiAxNzA5NDA4NDE5LCAic2VsbGluZ19wbGFuX2dyb3VwX2lkIjogODc5MTAwMDY3fV19&store_id=190268'
+      window.location.href = 'https://store.tennibot.com/tools/recurring/checkout_link?magic=eyJpdGVtcyI6IFt7ImlkIjogNDQzMTEyNzQ0MjI0MzUsICJxdWFudGl0eSI6IDEsICJzZWxsaW5nX3BsYW4iOiAxNzE2ODc5NTIzLCAic2VsbGluZ19wbGFuX2dyb3VwX2lkIjogODg2NDA3MzMxfV19&store_id=190268'
       return
     }
 
     if (product === 2) {
-      ReactGA.event({
-        category: 'set_checkout_option',
-        action: 'set_checkout_option',
-        value: 4
-      })
       window.location.href = 'https://tennibot.myshopify.com/cart/44311234936995:1?channel=buy_button'
     }
   }
-
-  useEffect(() => {
-    if (!ReactGA.isInitialized) {
-      ReactGA.initialize('G-5MY88GRQM4')
-    }
-  }, [])
 
   return (
     <section className="w-[80%] 2xl:w-[65%] mx-auto md:pt-16 pt-0 pb-20" >
         <div className="flex relative md:flex-row flex-col md:gap-0 gap-10 justify-between" >
             <div className="absolute md:block hidden top-[-15%] w-[50%] border-t-2 border-r-2 border-t-off-white rounded-r-lg h-[30%]" >
-                <h2 className="font-avenir uppercase text-t-off-white text-3xl bg-t-off-black pr-3 absolute top-[-15%]" >Monthly</h2>
+                <h2 className="font-avenir uppercase text-t-off-white text-3xl bg-t-off-black pr-3 absolute top-[-15%]" >Rent-to-own</h2>
             </div>
-            <div className="absolute md:block hidden top-[-15%] right-[13%] w-[17%] min-[1195px]:border-t-2 min-[1195px]:border-r-2 min-[1195px]:border-t-off-white rounded-r-lg h-[30%]" >
-                <h2 className="font-avenir uppercase text-t-off-white text-nowrap text-3xl bg-t-off-black pr-3 absolute top-[-15%]" >One-Time</h2>
+            <div className="absolute md:block hidden top-[-15%] right-[13%] w-[17%] md:border-t-2 md:border-r-2 md:border-t-off-white rounded-r-lg h-[30%]" >
+                <h2 className="font-avenir uppercase text-t-off-white text-nowrap text-3xl bg-t-off-black pr-3 absolute top-[-15%]" >Buy</h2>
             </div>
-            <h2 className="md:hidden font-avenir uppercase text-t-off-white text-3xl mb-[-1.2rem]" >Monthly</h2>
+            <h2 className="md:hidden font-avenir uppercase text-t-off-white text-3xl mb-[-1.2rem]" >Rent-to-own</h2>
             <button onClick={() => { setSelected(0) }} className={`${selected === 0 ? 'md:border-t-green' : 'border-t-off-white'} relative flex-[0_0_30%] border-2 md:hover:border-t-green rounded-lg px-5 mobilem:px-10 md:px-5 lg:px-7  py-5 flex flex-col justify-start transition-all duration-300 ease-in-out`} >
                 <div className="flex flex-col tablet:flex-row tablet:justify-between tablet:items-start items-start justify-between w-full" >
-                    <h3 className={titleStyle} >{`$${isChecked ? '89' : '79'} / Month`}</h3>
-                    <div className="w-full lg:absolute lg:right-0 tablet:w-[30%] tablet:min-w-20 relative flex flex-row tablet:gap-2 gap-0 tablet:flex-col items-center justify-between mb-5 transition-all duration-300 ease-in-out" >
+                    <h3 className={titleStyle} >{`$95 / Month`}</h3>
+                    {/* <div className="w-full lg:absolute lg:right-0 tablet:w-[30%] tablet:min-w-20 relative flex flex-row tablet:gap-2 gap-0 tablet:flex-col items-center justify-between mb-5 transition-all duration-300 ease-in-out" >
                         <label className={`tablet:hidden font-plutoLight text-xs ${!isChecked ? 'text-t-off-white' : 'text-t-disabled'}`} >Annual Plan</label>
                         <Switch checked={isChecked} className='transition-all mx-auto duration-300 ease-in-out' onCheckedChange={() => { setIsChecked(!isChecked) }} />
                         {
@@ -128,17 +100,18 @@ export default function BuySelection (): JSX.Element {
                                 </motion.div>
                         )}
                         <label className={`tablet:hidden font-plutoLight  text-xs ${isChecked ? 'text-t-off-white' : 'text-t-disabled'}`} >Monthly Plan</label>
-                    </div>
+                    </div> */}
                 </div>
                 <ul className="list-disc list-inside text-left flex flex-col gap-3 mt-4" >
-                    <li className={`font-plutoLight text-t-green text-pretty ${itemStyle}`} >Unlimited warranty</li>
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >White glove customer service</li>
                     {/* <li className={`font-plutoLight text-t-green text-pretty ${itemStyle}`} >Classic Membership Included</li> */}
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Cancel anytime</li>
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Own your Tennibot after 4 years</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Ball Collection</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Clay Court Sweeping</li>
-                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Video Recording</li>
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Unlimited Video Recording</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Real Time Line Judging</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Stat Tracking</li>
-                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Discounted buy out price<br /><a href="#faq" className='underline text-t-green' >*more details below</a></li>
                 </ul>
                 <div className="w-full flex flex-col items-center mt-10 md:hidden " >
                     <a onClick={() => {
@@ -154,16 +127,17 @@ export default function BuySelection (): JSX.Element {
             </button>
 
             <button onClick={() => { setSelected(1) }} className={`${selected === 1 ? 'md:border-t-green' : 'border-t-off-white'} bg-t-off-black relative flex-[0_0_30%] border-2 md:hover:border-t-green rounded-lg px-5 mobilem:px-10 md:px-5 lg:px-7 py-5 flex flex-col justify-start transition-all duration-300 ease-in-out`} >
-                <h3 className={`${titleStyle} mb-7 `} >$499 + $59 / Month</h3>
+                <h3 className={`${titleStyle} mb-7 `} >$995 / Year</h3>
                 <ul className="list-disc list-inside text-left flex flex-col gap-3" >
-                    <li className={`font-plutoLight text-t-green text-pretty ${itemStyle}`} >Unlimited warranty</li>
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >White glove customer service</li>
                     {/* <li className={`font-plutoLight text-t-green text-pretty ${itemStyle}`} >Classic Membership Included</li> */}
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Cancel anytime</li>
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Own your Tennibot after 4 years</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Ball Collection</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Clay Court Sweeping</li>
-                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Video Recording</li>
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Unlimited Video Recording</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Real Time Line Judging</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Stat Tracking</li>
-                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle} pb-2`} >Discounted buy out price<br /><a href="#faq" className='underline text-t-green' >*more details below</a></li>
                 </ul>
                 <div className="w-full flex flex-col items-center mt-10 md:hidden " >
                     <a onClick={() => {
@@ -178,15 +152,17 @@ export default function BuySelection (): JSX.Element {
                 </div>
             </button>
 
-            <h2 className="md:hidden font-avenir uppercase text-t-off-white text-3xl mb-[-2rem]" >One-Time</h2>
+            <h2 className="md:hidden font-avenir uppercase text-t-off-white text-3xl mb-[-2rem]" >Buy</h2>
             <button onClick={() => { setSelected(2) }} className={`${selected === 2 ? 'md:border-t-green' : 'border-t-off-white'} bg-t-off-black relative flex-[0_0_30%] border-2 md:hover:border-t-green rounded-lg px-5 mobilem:px-10 md:px-5 lg:px-7 py-5 flex flex-col justify-start transition-all duration-300 ease-in-out`} >
                 <h3 className={`${titleStyle} mb-7 `} >$2,995</h3>
                 <ul className="list-disc list-inside text-left flex flex-col gap-3" >
                     <li className={`font-plutoLight text-t-green text-pretty ${itemStyle}`} >2 Year Warranty</li>
                     {/* <li className={`font-plutoLight text-t-green text-pretty ${itemStyle}`} >Classic Membership Included</li> */}
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Free shipping in the US</li>
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >60 day money-back guarantee</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Ball Collection</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Clay Court Sweeping</li>
-                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Video Recording</li>
+                    <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Unlimited Video Recording</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Real Time Line Judging</li>
                     <li className={`font-plutoLight text-t-off-white text-pretty ${itemStyle}`} >Stat Tracking</li>
                     <br />
