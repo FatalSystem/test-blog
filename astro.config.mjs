0d536d2878864b960ae031a8718e9a98ee35e386
Jassel <ohjjmm@gmail.com>
2024-06-13 18:57:04 -0400

    chore: redirect /meeting to haithams calendly

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,8 @@ export default defineConfig({
     '/T5setup': 'https://youtu.be/BerTwqWqAPo',
     '/t5setup': 'https://youtu.be/BerTwqWqAPo',
     '/unbox': 'https://youtu.be/vvNnUARbGz0',
-    '/quickstart': 'https://www.youtube.com/watch?v=c7O39UYMls4'
+    '/quickstart': 'https://www.youtube.com/watch?v=c7O39UYMls4',
+    '/meeting': 'https://calendly.com/haithameletrabi/15-minute-call?month=2024-06'
   },
   integrations: [tailwind(), sitemap(), react({
     experimentalReactChildren: true
