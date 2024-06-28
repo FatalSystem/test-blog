567ed21e66f25e1ac8b0c53f075a3d32398729bd
Jassel <ohjjmm@gmail.com>
2024-06-27 21:44:11 -0400

    chore: add tennibot logo and add help redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,7 +23,8 @@ export default defineConfig({
     '/t5setup': 'https://youtu.be/BerTwqWqAPo',
     '/unbox': 'https://youtu.be/vvNnUARbGz0',
     '/quickstart': 'https://www.youtube.com/watch?v=c7O39UYMls4',
-    '/meeting': 'https://calendly.com/haithameletrabi/15-minute-call?month=2024-06'
+    '/meeting': 'https://calendly.com/haithameletrabi/15-minute-call?month=2024-06',
+    '/help': 'https://www.youtube.com/watch?v=BerTwqWqAPo'
   },
   integrations: [tailwind(), sitemap(), react({
     experimentalReactChildren: true
