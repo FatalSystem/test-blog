ef6103d9f2c2829b3c2f3b385dc5880802d29ccf
Jassel <ohjjmm@gmail.com>
2024-09-25 00:24:36 -0400

    chore: add offer banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,8 +8,8 @@ interface IBanner {
 export default function Banner ({ show, onClose }: IBanner): JSX.Element {
   return (
     <div className={` bg-t-dark-green font-avenirBold text-t-off-white flex flex-row items-center drop-shadow-lg transition-all duration-800 ease-in-out overflow-hidden ${show ? 'lg:h-14 h-16' : 'h-0'}`} >
-        <a href='https://tally.so/r/w2PgKp' target='_blank' rel="noreferrer" className='py-3 cursor-pointer ml-auto w-full text-left text-xs mobilem:text-sm mobilel:text-base mobilel:text-center sm:mr-0 mr-14 pl-5 2xl:text-lg '>
-            Take a <span className='underline text-t-green' >quick survey</span> to get a free wristband!
+        <a href='/buy' rel="noreferrer" className='py-3 cursor-pointer ml-auto w-full text-left text-xs mobilem:text-sm mobilel:text-base mobilel:text-center md:mr-0 sm:mr-20 mr-14 pl-5 2xl:text-lg '>
+          End-of-Season Sale Ends in 3 days!! Enter code TENNIBOT500 for $500 Off 🔥
         </a>
         <button className=' absolute z-10 right-[5%]' onClick={onClose} ><X className='size-8' /></button>
     </div>
