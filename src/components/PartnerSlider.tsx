244b92eb864e3e225d0b35910165be67dcbed64e
Jassel <ohjjmm@gmail.com>
2024-11-20 17:38:29 -0500

    chore: minor mobile improvements to partner landing, reserve page and add carousel to invest page

--- src/components/PartnerSlider.tsx
+++ src/components/PartnerSlider.tsx
@@ -2,10 +2,10 @@ import { TSliderTwo } from './TSliderTwo'
 
 const slides = [
   {
-    style: "bg-[url('/images/partner/founder-2.jpeg')] sm:h-[55vh] h-[45vh]"
+    style: "bg-[url('/images/invest/placeholderone.webp')] sm:h-[45vh] h-[45vh]"
   },
   {
-    style: "bg-[url('/images/partner/founder.jpg')] sm:h-[55vh] h-[45vh] bg-bottom"
+    style: "bg-[url('/images/invest/placeholdertwo.webp')] sm:h-[45vh] h-[45vh] bg-bottom"
   }
 ]
 // sm:h-[70vh] h-[45vh]
@@ -15,9 +15,9 @@ export default function PartnerSlider (): JSX.Element {
       slides={slides}
       contentContainerStyle="p-10 h-full sm:w-[70%]"
       // slideShades="bg-gradient-to-b from-[#232320]/[0.2] from-10% via-transparent via-70% to-[#232320]/[0.2] to-90%"
-    //   dotContainerStyle="sm:hidden"
-    //   dotContainerDesktopStyle="sm:flex bottom-[56vh]"
+      // dotContainerStyle="sm:hidden"
+      // dotContainerDesktopStyle="sm:flex bottom-[56vh]"
       arrowsStyle="bottom-[3%]"
-        />
+    />
   )
 }
