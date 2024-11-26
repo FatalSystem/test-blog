519856619e7ae56acd19f26bcd5dafe268aaec60
Jassel <ohjjmm@gmail.com>
2024-11-26 00:49:45 -0500

    feat: add thank you page + home page buttons changes

---
+++ functions/checkout.mts
@@ -0,0 +1,23 @@
+import type { Context } from '@netlify/functions'
+const Stripe = require('stripe')
+
+export default async (event: Request, context: Context): Promise<Response> => {
+  if (event.method !== 'GET') {
+    return new Response('Method Not Allowed', { status: 405 })
+  }
+
+  const STRIPE_KEY = Netlify.env.get('STRIPE_KEY')
+
+  try {
+    const data = await event.json()
+    const stripe = new Stripe(STRIPE_KEY)
+    const checkout = await stripe.checkout.sessions.retrieve(data.sessionId, {
+      apiKey: STRIPE_KEY
+    })
+
+    return new Response(JSON.stringify(checkout))
+  } catch (error) {
+    console.error(error)
+    return new Response('Error', { status: 500 })
+  }
+}
---
+++ src/components/ThankYouContent.tsx
@@ -0,0 +1,115 @@
+import { useEffect, useState } from 'react'
+import { CircleCheck, AlertCircle, Loader2 } from 'lucide-react'
+import tennibotLogo from '../assets/tennibot-logo.svg'
+import { Pages } from '@utils'
+
+interface SessionData {
+  currency_conversion: {
+    amount_total: number
+    fx_rate: number
+  }
+}
+
+export default function ThankYouContent (): JSX.Element {
+  const [sessionData, setSessionData] = useState<SessionData | null>(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchSessionData = async () => {
+      const urlParams = new URLSearchParams(window.location.search)
+      const sessionId = urlParams.get('session')
+
+      if (!sessionId) {
+        setError('No session ID found')
+        setIsLoading(false)
+        return
+      }
+
+      try {
+        const response = await fetch('/.netlify/functions/checkout', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ sessionId })
+        })
+
+        if (!response.ok) {
+          throw new Error('Payment verification failed')
+        }
+
+        const data = await response.json() as SessionData
+        const amount = (parseInt((data?.currency_conversion?.amount_total ?? 49900).toFixed(2) * (data?.currency_conversion?.fx_rate ?? 1)) / 100) ?? 499.00
+        const currency = data?.currency?.toUpperCase() ?? 'USD'
+        gtag('event', 'conversion', { send_to: 'AW-16667981876/mKp3CMnUsckZELTw9Is-', value: amount, currency, transaction_id: sessionId })
+        setSessionData(data)
+      } catch (err) {
+        setError('Unable to verify payment. Please contact support.')
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    void fetchSessionData()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Loader2 className="animate-spin" color="white" size={40} />
+      </div>
+    )
+  }
+
+  const amount = (parseInt((sessionData?.currency_conversion?.amount_total ?? 49900).toFixed(2) * (sessionData?.currency_conversion?.fx_rate ?? 1)) / 100) ?? 499.00
+  const currency = sessionData?.currency?.toUpperCase() ?? 'USD'
+
+  return (
+    <main className="overflow-hidden text-t-off-white">
+        <section className="mx-auto md:h-[100vh] h-[150vh] flex md:flex-row flex-col-reverse justify-center">
+            <div className="flex-col flex bg-t-off-black md:w-[50%] w-full h-full justify-start mt-[15vh]">
+                <div className="flex w-[80%] mx-auto flex-col justify-center mb-8">
+                    <div>
+                        <img src={tennibotLogo.src} width={30} alt="Tennibot Logo" />
+                        <p className="font-avenir text-t-gray mt-3">Pay Tennibot</p>
+                        <p className="font-avenirBold text-t-off-white text-2xl mt-3">${amount} {currency}</p>
+                    </div>
+                    <div className="mt-5 border-b-[1px] pb-10 border-[#afafae5f] " >
+                        <div className="flex flex-row justify-between" >
+                            <p className="font-avenir text-t-off-white">Partner</p>
+                            <p className="font-avenir text-t-off-white">${amount} {currency}</p>
+                        </div>
+                        <p className="font-avenir text-t-gray text-sm mt-3">Thank you for you reservation. The expected shipping date is April 2025.</p>
+                        <div className="bg-[#afafae5f] rounded-md px-2 py-[1.5px] flex flex-col justify-center mt-3 w-fit ">
+                            <p className="font-avenir text-t-off-white text-sm" >Qty {Math.ceil(((parseInt(sessionData?.currency_conversion?.amount_subtotal ?? 49900) / 100).toFixed(2)) / 499.00) || 1}</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-row justify-between mt-5" >
+                        <p className="font-avenir text-t-off-white">Total paid</p>
+                        <p className="font-avenir text-t-off-white">${amount} {currency}</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col bg-t-off-white md:w-[50%] w-full h-full justify-start pt-[15vh]">
+                <div className="w-[80%] mx-auto flex flex-col justify-center">
+                    <div className="flex flex-col justify-center items-center">
+                        <CircleCheck color="green" size={40}/>
+                        <h1 className="font-avenirBold text-t-off-black text-2xl mt-3">Thank you!</h1>
+                        <p className="font-avenir text-t-gray mt-3">A payment to TENNIBOT will appear on your statement.</p>
+                        <div className="bg-[#afafae5f] rounded-md px-5 py-5 flex flex-row justify-between mt-3 w-full">
+                            <p className="font-avenirBold uppercase text-[#595959dc]">Tennibot</p>
+                            <p className="font-avenir text-[#595959dc]">${amount.toFixed(2)} {currency}</p>
+                        </div>
+                        <div className="flex flex-row gap-5">
+                            <a href={Pages.TERMS} className="font-avenir text-t-gray mt-3">Terms</a>
+                            <a href={Pages.PRIVACY} className="font-avenir text-t-gray mt-3">Privacy</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+  )
+}
