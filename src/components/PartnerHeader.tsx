dd358a781478c612f41b183f7542aac1bd5275ab
Jassel <ohjjmm@gmail.com>
2024-11-29 01:41:39 -0500

    chore: remove banner and partner bottom box

--- src/components/PartnerHeader.tsx
+++ src/components/PartnerHeader.tsx
@@ -40,7 +40,7 @@ export default function PartnerHeader (): JSX.Element {
 
   return (
     <div className='sticky top-0 z-50'>
-      <Banner show={showBanner} onClose={closeBanner} />
+      {/* <Banner show={showBanner} onClose={closeBanner} /> */}
       <header className='relative' >
         <AnimatePresence >
           {open && (
