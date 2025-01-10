64035b289b0c844d58b1faf9ea883fbd5a9c6a5d
Jassel <ohjjmm@gmail.com>
2025-01-10 00:37:34 -0400

    chore: configure api host to proxy server

--- src/hooks/useMixpanel.tsx
+++ src/hooks/useMixpanel.tsx
@@ -3,6 +3,7 @@ import mixpanel from 'mixpanel-browser'
 export const useMixpanel = (): { initialize: () => void, trackEvent: (event: string, data: any) => void } => {
   const initialize = (): void => {
     mixpanel.init('16d06bcdf9b0f6e260ccb622aa0cc19a', {
+      api_host: 'https://mixpanel-tracking-proxy-625719360183.us-central1.run.app/',
       debug: true,
       track_pageview: true,
       persistence: 'localStorage'
