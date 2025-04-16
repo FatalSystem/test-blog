6855faa6129a8ac4c0260aa7a2b8eabf2ecbc623
Jassel <ohjjmm@gmail.com>
2025-04-15 23:20:08 -0400

    chore: add distinct id test

--- src/components/ReserveButtonTest.tsx
+++ src/components/ReserveButtonTest.tsx
@@ -80,70 +80,67 @@ export default function ReserveButtonTest (): JSX.Element {
   //     return () => { clearTimeout(timer) }
   //   }, [])
 
-  const fetchClientSecret = useCallback(async () => {
-    // Create a Checkout Session
-    const urlParams = new URLSearchParams(window.location.search)
-    const utmParams = {
-      campaign: urlParams.get('utm_campaign'),
-      content: urlParams.get('utm_content'),
-      id: urlParams.get('utm_id'),
-      medium: urlParams.get('utm_medium'),
-      source: urlParams.get('utm_source'),
-      term: urlParams.get('utm_term')
-    }
-    return await fetch('/.netlify/functions/create-checkout-test', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        //   distinctId,
-        utmParams
-      })
-    }).then(async (res) => {
-      const resp = await res.json()
-      return resp
-    }).then((data) => data.client_secret)
-  }, [])
+  // const fetchClientSecret = useCallback(async () => {
+  //   // Create a Checkout Session
+  //   const urlParams = new URLSearchParams(window.location.search)
+  //   const utmParams = {
+  //     campaign: urlParams.get('utm_campaign'),
+  //     content: urlParams.get('utm_content'),
+  //     id: urlParams.get('utm_id'),
+  //     medium: urlParams.get('utm_medium'),
+  //     source: urlParams.get('utm_source'),
+  //     term: urlParams.get('utm_term')
+  //   }
+  //   return await fetch('/.netlify/functions/create-checkout-test', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({
+  //       //   distinctId,
+  //       utmParams
+  //     })
+  //   }).then(async (res) => {
+  //     const resp = await res.json()
+  //     return resp
+  //   }).then((data) => data.client_secret)
+  // }, [])
 
-  // Call your backend to set shipping options
-  const onShippingDetailsChange = async (shippingDetailsChangeEvent) => {
-    const { checkoutSessionId, shippingDetails } = shippingDetailsChangeEvent
-    const response = await fetch('/.netlify/functions/calculate-shipping', {
-      method: 'POST',
-      body: JSON.stringify({
-        checkout_session_id: checkoutSessionId,
-        shipping_details: shippingDetails
-      })
-    })
+  // // Call your backend to set shipping options
+  // const onShippingDetailsChange = async (shippingDetailsChangeEvent) => {
+  //   const { checkoutSessionId, shippingDetails } = shippingDetailsChangeEvent
+  //   const response = await fetch('/.netlify/functions/calculate-shipping', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       checkout_session_id: checkoutSessionId,
+  //       shipping_details: shippingDetails
+  //     })
+  //   })
 
-    if (response.type === 'error') {
-      return await Promise.resolve({ type: 'reject', errorMessage: response.message })
-    } else {
-      return await Promise.resolve({ type: 'accept' })
-    }
+  //   if (response.type === 'error') {
+  //     return await Promise.resolve({ type: 'reject', errorMessage: response.message })
+  //   } else {
+  //     return await Promise.resolve({ type: 'accept' })
+  //   }
+  // }
+
+  // const options = { fetchClientSecret, onShippingDetailsChange }
+
+  const handleTest = () => {
+    const distinctId = getDistinctId()
+    console.log('Distinct ID:', distinctId)
   }
-
-  const options = { fetchClientSecret, onShippingDetailsChange }
 
   return (
     <div id="checkout" className="p-4">
-        <EmbeddedCheckoutProvider
+        {/* <EmbeddedCheckoutProvider
         stripe={stripePromise}
         options={options}
       >
         <EmbeddedCheckout />
-      </EmbeddedCheckoutProvider>
-        {/* <a
-        data-tracking="partner-reserve-button"
-        onClick={handleCreateCheckout}
-        className={`w-72 cursor-pointer text-xl 2xl:text-2xl 2xl:py-3 rounded-full py-2 font-avenir uppercase text-t-green border-2 transition duration-300 text-center bg-red-500 border-t-green hover:bg-t-green hover:text-t-off-black ${isLoading ? 'opacity-50 cursor-wait' : ''}`}
-        >
-        <span className="flex  place-items-center justify-center">
-            Reserve
-            {isLoading && <Loader2 className="ml-2 size-4 mb-1 animate-spin " />}
-        </span>
-        </a> */}
+      </EmbeddedCheckoutProvider> */}
+      <button onClick={handleTest}>Get distinct ID</button>
+
     </div>
   )
 }
