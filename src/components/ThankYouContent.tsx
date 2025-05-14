52b2da46c6436057f781bf4f36b06a5489495661
Jassel <ohjjmm@gmail.com>
2025-05-13 23:46:15 -0400

    chore: fix partner description in the thank you page

--- src/components/ThankYouContent.tsx
+++ src/components/ThankYouContent.tsx
@@ -102,7 +102,7 @@ export default function ThankYouContent (): JSX.Element {
                             <p className="font-avenir text-t-off-white">{productType === 'rover' ? 'Rover' : 'Partner'}</p>
                             <p className="font-avenir text-t-off-white">${sessionData?.value ?? 499.00} {sessionData?.currency ?? 'USD'}</p>
                         </div>
-                        <p className="font-avenir text-t-gray text-sm mt-3">{productType === 'rover' ? 'Autonomous Ball Collector' : 'Reserve for only $499 today to secure your partner. Pay the remaining $1595 before shipping in May 2025.'}</p>
+                        <p className="font-avenir text-t-gray text-sm mt-3">{productType === 'rover' ? 'Autonomous Ball Collector' : 'Smart Ball Machine'}</p>
                         {productType !== 'rover' &&
                           <div className="bg-[#afafae5f] rounded-md px-2 py-[1.5px] flex flex-col justify-center mt-3 w-fit ">
                             <p className="font-avenir text-t-off-white text-sm" >Qty {sessionData?.quantity ?? 1}</p>
